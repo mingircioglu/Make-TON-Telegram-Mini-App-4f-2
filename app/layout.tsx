2ebb70107993f35c151ef0cb857942510b63cd68
mingircioglu <ozalozal4@gmail.com>
2024-09-28 02:13:21 +0300

    tonrename

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-'use client'
+    'use client'
 
 import "./globals.css";
 import { TonConnectUIProvider } from "@tonconnect/ui-react";
@@ -14,7 +14,7 @@ export default function RootLayout({
         <title>TON Connect Demo</title>
       </head>
       <body>
-        <TonConnectUIProvider manifestUrl="https://violet-traditional-rabbit-103.mypinata.cloud/ipfs/QmQJJAdZ2qSwdepvb5evJq7soEBueFenHLX3PoM6tiBffm">
+        <TonConnectUIProvider manifestUrl="https://emerald-abstract-vole-714.mypinata.cloud/ipfs/QmYdwjuo3x4nzG4Hd95RDRe2RxeA9AzwvwAEamPyEuXeWa">
           {children}
         </TonConnectUIProvider>
       </body>
